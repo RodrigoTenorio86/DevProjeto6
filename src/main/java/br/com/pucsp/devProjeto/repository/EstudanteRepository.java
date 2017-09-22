3b7f29d022e7534f40fe7b612c9528bd1919c97d
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2017-09-21 21:17:18 -0300

    criador query "select e from Estudante e where e.nome := nome" na interface EstudanteRepository

--- src/main/java/br/com/pucsp/devProjeto/repository/EstudanteRepository.java
+++ src/main/java/br/com/pucsp/devProjeto/repository/EstudanteRepository.java
@@ -2,6 +2,7 @@ package br.com.pucsp.devProjeto.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.pucsp.devProjeto.modelo.Estudante;
@@ -10,4 +11,7 @@ public interface EstudanteRepository extends CrudRepository<Estudante, Long>{
 public List<Estudante> findByNome(String nome);
 public List<Estudante> findByNomeIgnoreCaseContaining(String nome);
 public List<Estudante> findByEmail(String email);
+@Query("select e from Estudante e where e.nome := nome")
+public List<Estudante> buscarPorNome(@Param("nome")String nome);
+
 }
