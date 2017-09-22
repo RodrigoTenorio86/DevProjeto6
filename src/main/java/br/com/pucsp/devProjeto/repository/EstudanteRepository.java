49b00cac546b08ffb7387559a6803294e7fd8508
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2017-09-22 14:07:48 -0300

    mudador extençao para JpaRepository e retirador query"select e from Estudante e where e.nome := nome"

--- src/main/java/br/com/pucsp/devProjeto/repository/EstudanteRepository.java
+++ src/main/java/br/com/pucsp/devProjeto/repository/EstudanteRepository.java
@@ -2,17 +2,21 @@ package br.com.pucsp.devProjeto.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import br.com.pucsp.devProjeto.modelo.Estudante;
 
-public interface EstudanteRepository extends CrudRepository<Estudante, Long>{
+//PagingAndSortingRepository   CrudRepository  JpaRepository
+public interface EstudanteRepository extends JpaRepository<Estudante, Long>{
 public List<Estudante> findByNome(String nome);
 public List<Estudante> findByNomeIgnoreCaseContaining(String nome);
 public List<Estudante> findByEmail(String email);
-@Query("select e from Estudante e where e.nome := nome")
-public List<Estudante> buscarPorNome(@Param("nome")String nome);
+//@Query("select e from Estudante e where e.nome := nome")
+//public List<Estudante> buscarPorNome(@Param("nome")String nome);
 
 }
